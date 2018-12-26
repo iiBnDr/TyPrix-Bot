5af660fa3af02aaafbf6fea660f7e21bc30a3e6c
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2018-12-26 04:38:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,41 @@
-﻿const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require('discord.js');
+
+const Util = require('discord.js');
+
+const getYoutubeID = require('get-youtube-id');
+
+const Canvas = require('canvas');
+
+const jimp = require('jimp');
+
+const convert = require("hh-mm-ss");
+
+const dateFormat = require('dateformat');
+
+const fetchVideoInfo = require('youtube-info');
+
+const YouTube = require('simple-youtube-api');
+
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+
+const queue = new Map();
+
+const ytdl = require('ytdl-core');
+
+const fs = require('fs');
+
 const moment = require('moment');
-var prefix = "$";
-client.on("message", message => {
+
+const gif = require("gif-search");
+
+const client = new Discord.Client({disableEveryone: true});
+
+
+const prefix = "#";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
             if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
