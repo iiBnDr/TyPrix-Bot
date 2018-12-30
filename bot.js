a490d510c02918e062eeb33e34bf3cbaacf6d966
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2018-12-30 18:30:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const moment = require("moment");  
-const fs = require("fs");      
-const dateFormat = require('dateformat');
-const client = new Discord.Client(); 
-const Canvas = require("canvas"); //npm i canvas
 const prefix = "$" // برفكس
 var adminprefix = '$'
 var prefix = "$";
