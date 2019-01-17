4dbfe556b06b70fb34b0c1f77974e68d801877d9
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2019-01-17 13:52:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -605,4 +605,23 @@ const Sra7a = [
 });
 
 
+client.on("message", msg => {
+  var prefix = '$'//البركفس
+  if(msg.content.startsWith(prefix + 'myaccount')){
+    let embed = new Discord.RichEmbed()
+    .setThumbnail(msg.author.displayAvatarURL)
+    .setColor("RANDOM")
+    .addField("Year📆",msg.author.createdAt.getFullYear())
+    .addField("Hour📆", msg.author.createdAt.getHours())
+    .addField("Day📆", msg.author.createdAt.getDay())
+    .addField("Month📆", msg.author.createdAt.getMonth())
+    .addField("Minutes📆", msg.author.createdAt.getMinutes())
+    .addField("Seconds📆", msg.author.createdAt.getSeconds())
+    .addField("Full📆", msg.author.createdAt.toLocaleString())
+    .setTimestamp()
+    msg.channel.send(embed);
+  }
+});
+
+
 client.login(process.env.BOT_TOKEN);
