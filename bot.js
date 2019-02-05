883ce5da890d0533670d9785db7f881055177a58
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2019-02-05 15:40:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const moment = require("moment");
+const fs = require("fs");
 const ms = require('ms')
 const fetch = require('node-fetch');
 var adminprefix = '$'
