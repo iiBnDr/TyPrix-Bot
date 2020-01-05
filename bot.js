29259cddb75aec471c75d3d7624406cbaa3df46d
iiBnDr <45108040+iiBnDr@users.noreply.github.com>
2020-01-05 03:00:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const moment = require("moment");
-const ms = require("ms");
+const moment = require('moment');
+const ms = require('ms');
 var adminprefix = '$'
 var prefix = "$";
 
